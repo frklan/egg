7ea54815e2700d9b3af8523e69af83abb7a90cbb
Fredrik Andersson <fredrik.k.andersson@gmail.com>
2018-12-09 12:27:52 +0100

    Added sdl framework

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-clang++ -std=c++1z -g -Wall src/app.cpp src/timer.cpp src/main.cpp -o egg \
+clang++ -std=c++1z -g -Wall src/app.cpp src/timer.cpp src/sdl.cpp src/main.cpp -o egg \
   -I /usr/local/include -L /usr/local/lib \
   -I$(brew --prefix boost)/include -L$(brew --prefix boost)/lib \
-  -lboost_program_options 
+  -lboost_program_options \
+  -I /Library/Frameworks/SDL2.framework/Headers \
+  -I /Library/Frameworks/SDL2_mixer.framework/Headers \
+  -framework SDL2  -framework SDL2_mixer
